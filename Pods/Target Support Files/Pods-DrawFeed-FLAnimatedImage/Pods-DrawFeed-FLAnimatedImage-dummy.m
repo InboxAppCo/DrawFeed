c1d999aeabaaad29797dd2bfd3741d71372da8d9
Hani Shabsigh <hanishabsigh@gmail.com>
2015-08-02 22:01:14 -0400

    removed pods, added gitignore

--- Pods/Target Support Files/Pods-DrawFeed-FLAnimatedImage/Pods-DrawFeed-FLAnimatedImage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DrawFeed_FLAnimatedImage : NSObject
-@end
-@implementation PodsDummy_Pods_DrawFeed_FLAnimatedImage
-@end
--- Pods/Target Support Files/Pods-DrawFeed/Pods-DrawFeed-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DrawFeed : NSObject
-@end
-@implementation PodsDummy_Pods_DrawFeed
-@end
